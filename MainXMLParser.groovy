ed5a0340d902abaef835bbc8ca5a45b3a6c23829
Vignesh-Sakkarai <31211400+Vignesh-Sakkarai@users.noreply.github.com>
2019-02-12 11:19:34 +0100

    Add files via upload
    Required Java and Groovy dependencies
    http://www.java2s.com/Code/JarDownload/groovy-all/groovy-all-2.1.3.jar.zip

---
+++ MainXMLParser.groovy
@@ -0,0 +1,32 @@
+import groovy.xml.XmlUtil
+
+class MainXMLParser {
+
+    static void main(String[] args) {
+
+      def xmlfilePath = "/Users/vignesh/Desktop/.xml"
+
+      def xml = new XmlParser().parse(xmlfilePath)
+
+      //pages[0] is outer tag, page is inner tag, pageType is child of inner
+      <!--EG
+          <pages>
+            <page>
+              <pageType>DESKTOP</pageType>
+            </page>
+            <page>
+              <pageType>MOBILE</pageType>
+            </page>
+          </pages>
+        -->
+        //If the pagType is DESKTOP, the below code will change in to MOBILE and update the given xml file.
+
+        xml.pages[0].page.each {
+            ob -> ob.pageType[0].text().equals("DESKTOP")
+        }.each {
+            ob -> ob.pageType[0].value = "MOBILE"
+        }
+        XmlUtil xmlUtil = new XmlUtil()
+        xmlUtil.serialize(xml, new FileWriter(xmlfilePath))
+    }
+}
