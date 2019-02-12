06cd0f3445cb260c639e94bfff587fbfa5f4b482
Vignesh-Sakkarai <31211400+Vignesh-Sakkarai@users.noreply.github.com>
2019-02-12 11:51:51 +0100

    Update MainXMLParser.groovy

--- MainXMLParser.groovy
+++ MainXMLParser.groovy
@@ -8,8 +8,8 @@ class MainXMLParser {
 
       def xml = new XmlParser().parse(xmlfilePath)
 
-      //pages[0] is outer tag, page is inner tag, pageType is child of inner
-      <!--EG
+    /** pages[0] is outer tag, page is inner tag, pageType is child of inner
+         EG
           <pages>
             <page>
               <pageType>DESKTOP</pageType>
@@ -18,8 +18,9 @@ class MainXMLParser {
               <pageType>MOBILE</pageType>
             </page>
           </pages>
-        -->
-        //If the pagType is DESKTOP, the below code will change in to MOBILE and update the given xml file.
+
+        If the pagType is DESKTOP, the below code will change in to MOBILE and update the given xml file.
+     **/
 
         xml.pages[0].page.each {
             ob -> ob.pageType[0].text().equals("DESKTOP")
